acb80a13dca36776305925a8d972edf9585f8ce6
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-07-24 10:48:12 +0200

    WIP

--- test/provider_spec.js
+++ test/provider_spec.js
@@ -7,6 +7,6 @@ var providers = require('../lib/providers')(lodash.noop)
 
 describe('providers', function () {
   it('works fine', function (done) {
-    providers[0].start(done)
+    providers[0](done)
   })
 })
