4e48b27700b3a1546a05a0529696a228e5148c94
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-08-02 11:26:03 +0200

    Normalize Pricing symbol

--- core/extract/price.js
+++ core/extract/price.js
@@ -5,17 +5,27 @@ var lodash = require('lodash')
 /**
  * Integer price value with optional euro symbol
  * @example
- * 80e
- * 150E
- * 200€
+ * 80e → 80€
+ * 150E → 150€
+ * 200€ → 200€
  * @type {RegExp}
  */
-var priceRegex = /[0-9]+[ ]?[€eE]/
-var whitespaceRegex = /\s/g
+
+var REGEX_PRICE = /[0-9]+[ ]?[€eE]/
+var REGEX_WHITESPACE = /\s/g
+var REGEX_SYMBOL = /[€eE]/
+
+function getFirstMatch (str, regex) {
+  return lodash.first(str.match(regex))
+}
+
+function normalizePrice (str) {
+  return str.replace(REGEX_WHITESPACE, '').replace(REGEX_SYMBOL, '€')
+}
 
 function extractPrice (str) {
-  var result = lodash.first(str.match(priceRegex))
-  return result && result.replace(whitespaceRegex, '')
+  var price = getFirstMatch(str, REGEX_PRICE)
+  return price && normalizePrice(price)
 }
 
 module.exports = extractPrice
--- test/extract_spec/common.js
+++ test/extract_spec/common.js
@@ -9,6 +9,17 @@ module.exports = function (extract) {
   })
   it('price', function () {
     should(extract.price('')).be.undefined()
-    extract.price('Vendo Loft Blade 7,8 2011 - 200€').should.be.equal('200€')
+
+    ;['80e', '80 e', ' 80 e', '80 e ', ' 80 e '].forEach(function (price) {
+      extract.price(price).should.be.equal('80€')
+    })
+
+    ;['150E', '150 E', ' 150 E', '150 E ', ' 150 E '].forEach(function (price) {
+      extract.price(price).should.be.equal('150€')
+    })
+
+    ;['200€', '200 €', ' 200 €', ' 200 € ', '200 € '].forEach(function (price) {
+      extract.price(price).should.be.equal('200€')
+    })
   })
 }
