1b057b149b7de2f4f9e27e65c7bff9eca58901d9
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-08-22 10:30:03 +0200

    Delete unnecessary code

--- core/schema/clean-title/index.js
+++ core/schema/clean-title/index.js
@@ -1,13 +1,4 @@
 'use strict'
 
 var lodash = require('lodash')
-var WORDS = require('./words.json')
-var regex = new RegExp(WORDS.join('|'), 'i')
-
-function trimWords (str) {
-  return lodash.replace(str, regex, '')
-}
-
-trimWords.regex = regex
-
 module.exports = lodash.flow(lodash.trim)
