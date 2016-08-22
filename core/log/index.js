98c09fc1cdc25035cab55e9de19c93ce857ecb0b
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-08-22 09:44:37 +0200

    Silent logs on prod

--- core/log/index.js
+++ core/log/index.js
@@ -5,6 +5,7 @@
  */
 
 var Acho = require('acho')
+var isProduction = process.env.NODE_ENV === 'production'
 
 var loggers = {
   sync_providers: {
@@ -13,6 +14,7 @@ var loggers = {
   },
   totalwind: {
     keyword: 'totalwind'
+    level: isProduction ? 'muted' : 'all'
   },
   sail_extractor: {
     keyword: 'sail_extractor'
