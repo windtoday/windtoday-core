e9b1dd25a07af910295b82f209603ed650ac8a72
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-04 17:55:30 +0200

    Little refactor

--- core/providers/totalwind/extractor.js
+++ core/providers/totalwind/extractor.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const { price, year, sail } = require('../../identify')
 const isBlacklisted = require('../../schema/is-blacklisted')
-const extract = require('../../identify')
 const { toLower, isEqual, assign, merge, omit } = require('lodash')
 
 const CONST = {
   SOURCE_NAME: 'totalwind',
   IGNORE_LOG_PROPS: ['updatedAt', 'createdAt', 'link', 'title', 'provider']
+}
+
+const getExtractor = {
+  sails: sail
+}
+
+function log (data) {
+  this.log.debug(omit(data, CONST.IGNORE_LOG_PROPS))
 }
 
 function createExtractor (type, category) {
@@ -20,26 +28,29 @@ function createExtractor (type, category) {
     const normalizeTitle = toLower(data.title)
 
     const dataExtract = {
-      price: extract.price(normalizeTitle),
-      year: extract.year(normalizeTitle)
+      price: price(normalizeTitle),
+      year: year(normalizeTitle)
     }
 
-    if (isEqual(category, 'sails')) assign(dataExtract, extract.sail(normalizeTitle))
+    var extractor = getExtractor[category]
+    if (extractor) assign(dataExtract, sail(normalizeTitle))
+
     merge(data, dataExtract)
 
-    const self = this
+    this.validate(data, (validationError, instance) => {
+      ++this.stats.total
 
-    this.validate(data, function (validationError, instance) {
-      ++self.stats.total
-      if (!validationError) {
-        self.log.debug(omit(instance, CONST.IGNORE_LOG_PROPS))
-        ++self.stats.valid
-        if (self.db[category]) {
-          ++self.stats.add
-          self.db[category].addObject(instance)
-        }
-      } else {
-        self.log.error(validationError)
+      if (validationError) {
+        this.log.error(validationError)
+        return
+      }
+
+      log.call(this, instance)
+
+      ++this.stats.valid
+      if (this.db[category]) {
+        ++this.stats.add
+        this.db[category].addObject(instance)
       }
     })
   }
