251c01953823111bc30265fb1e3d4a202b7efc7a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-04 18:44:19 +0200

    Remove log

--- test/identify_spec/board/brand.js
+++ test/identify_spec/board/brand.js
@@ -4,8 +4,6 @@ const should = require('should')
 const { get } = require('lodash')
 
 module.exports = function (identify) {
-  console.log(identify)
-
   describe('brand', function () {
     it('not detect', function () {
       const brand = get(identify.board(''), 'brand')
