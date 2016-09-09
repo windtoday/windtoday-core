a612e9234991812e1fc1a455be1ccac8de9e8ea4
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-09 11:00:36 +0200

    Inject log as dependency

--- core/identify/board/index.js
+++ core/identify/board/index.js
@@ -1,36 +1,3 @@
 'use strict'
-
 const log = require('../../log')('board_unidentify')
-const logUnmatching = require('../../util/log-unmatching')('board', log)
-const { boards } = require('../../directory')
-const getLitres = require('./litres')
-
-function createAdd (key, fnValue) {
-  function add (acc) {
-    const value = fnValue(acc)
-    if (value) acc.output[key] = value
-    else logUnmatching(key, acc)
-  }
-
-  return add
-}
-
-const addLitres = createAdd('litres', (acc) => getLitres(acc.input))
-const addBrand = createAdd('brand', (acc) => acc.dir.brand())
-const addModel = createAdd('model', (acc) => acc.dir.model())
-
-function board (str) {
-  const acc = {
-    dir: boards(str),
-    input: str,
-    output: {}
-  }
-
-  addLitres(acc)
-  addBrand(acc)
-  addModel(acc)
-
-  return acc.output
-}
-
-module.exports = board
+module.exports = require('./factory')(log)
--- core/identify/sail/index.js
+++ core/identify/sail/index.js
@@ -1,36 +1,3 @@
 'use strict'
-
 const log = require('../../log')('sail_unidentify')
-const logUnmatching = require('../../util/log-unmatching')('sail', log)
-const { sails } = require('../../directory')
-const getSize = require('./size')
-
-function createAdd (key, fnValue) {
-  function add (acc) {
-    const value = fnValue(acc)
-    if (value) acc.output[key] = value
-    else logUnmatching(key, acc)
-  }
-
-  return add
-}
-
-const addSize = createAdd('size', (acc) => getSize(acc.input))
-const addBrand = createAdd('brand', (acc) => acc.dir.brand())
-const addModel = createAdd('model', (acc) => acc.dir.model())
-
-function sail (str) {
-  const acc = {
-    dir: sails(str),
-    input: str,
-    output: {}
-  }
-
-  addSize(acc)
-  addBrand(acc)
-  addModel(acc)
-
-  return acc.output
-}
-
-module.exports = sail
+module.exports = require('./factory')(log)
