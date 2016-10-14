aff20f6243924b82f79d8918729bb1390639b064
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-14 22:55:59 +0200

    Avoid mixin extractor

--- core/providers/totalwind/extractor/specific.js
+++ core/providers/totalwind/extractor/specific.js
@@ -1,28 +1,15 @@
 'use strict'
 
 const boardFactory = require('../../../identify/board')
-const mixinFactory = require('../../../identify/mixin')
 const sailFactory = require('../../../identify/sail')
 const createLogger = require('../../../log')
 const { noop } = require('lodash')
 
-const log = createLogger('totalwind_extractor_specific')
-
 function specificExtractor (opts) {
   const { category, loggerKeyword } = opts
-
-  if (category === 'sails') {
-    return sailFactory(createLogger(`${loggerKeyword}_unidentify`))
-  }
-
-  if (category === 'boards') {
-    return boardFactory(createLogger(`${loggerKeyword}_unidentify`))
-  }
-
-  return mixinFactory({
-    sailLogger: createLogger(`${loggerKeyword}_mixin_unidentify`),
-    boardLogger: createLogger(`${loggerKeyword}_mixin_unidentify`)
-  })
+  if (category === 'sails') return sailFactory(createLogger(`${loggerKeyword}_unidentify`))
+  if (category === 'boards') return boardFactory(createLogger(`${loggerKeyword}_unidentify`))
+  return noop
 }
 
 module.exports = specificExtractor
