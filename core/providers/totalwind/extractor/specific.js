e3d0da1f9ff58d3e6ff948fe86b728830f6903f3
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-20 11:00:34 +0200

    Mixin as fallback

--- core/providers/totalwind/extractor/specific.js
+++ core/providers/totalwind/extractor/specific.js
@@ -7,9 +7,19 @@ const { noop } = require('lodash')
 
 function specificExtractor (opts) {
   const { category, loggerKeyword } = opts
-  if (category === 'sails') return sailFactory(createLogger(`${loggerKeyword}_unidentify`))
-  if (category === 'boards') return boardFactory(createLogger(`${loggerKeyword}_unidentify`))
-  return noop
+
+  if (category === 'sails') {
+    return sailFactory(createLogger(`${loggerKeyword}_unidentify`))
+  }
+
+  if (category === 'boards') {
+    return boardFactory(createLogger(`${loggerKeyword}_unidentify`))
+  }
+
+  return mixinFactory({
+    sailLogger: createLogger(`${loggerKeyword}_mixin_unidentify`),
+    boardLogger: createLogger(`${loggerKeyword}_mixin_unidentify`)
+  })
 }
 
 module.exports = specificExtractor
