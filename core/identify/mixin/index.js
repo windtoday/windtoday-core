fa46fd4f72d598db294d740206d3e1f2006a6d54
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-20 10:22:15 +0200

    Improve mixin detection

--- core/identify/mixin/index.js
+++ core/identify/mixin/index.js
@@ -1,22 +1,24 @@
 'use strict'
 
-const boardFactory = require('./board')
-const sailFactory = require('./sail')
-
-const REGEX_EXCLUDE = /fin|aleta|m[aá]st/
+const boardFactory = require('../board')
+const sailFactory = require('../sail')
+const REGEX_BLACKLIST = RegExp(require('./words.json').join('|'), 'i')
 
 function factory (opts) {
   const sail = sailFactory(opts.sailLogger)
   const board = boardFactory(opts.boardLogger)
 
+  /**
+   * Detect board/sail from not determinated input.
+   */
   function mixin (str) {
-    if (REGEX_EXCLUDE.test(str)) return
+    if (REGEX_BLACKLIST.test(str)) return
 
     var boardExtracted = board(str)
     if (boardExtracted.model) return boardExtracted
 
     var sailExtracted = sail(str)
-    if (sail.model) return sailExtracted
+    if (sailExtracted.model) return sailExtracted
     if (sailExtracted.brand) return sailExtracted
 
     return boardExtracted
