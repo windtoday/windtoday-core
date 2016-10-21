93502fcf86368db8dfde4d0dc36ac2bc6cbd16ec
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-21 19:21:42 +0200

    Fix typo

--- core/schema/clean-title/index.js
+++ core/schema/clean-title/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
+const cleanWhiteSpaces = require('condense-whitespace')
 const { flow } = require('lodash')
 const titleize = require('titleize')
-var cleanWhiteSpaces = require('condense-whitespace')
 
 const REGEX_WORDS = RegExp(require('./words.json').join('|'), 'ig')
 const REGEX_PRICE = require('../../identify/price').regex
