ec7cdce40accb8132a74eca73add009d89960d1e
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-21 17:36:05 +0200

    Refactor worker API

--- core/worker/index.js
+++ core/worker/index.js
@@ -11,33 +11,48 @@ const db = require('../db')
 
 const CONST = {
   CHECK_HOSTS: require('config').check_hosts,
-  REQUIRED_PARAMS: [ 'provider' ]
+  REQUIRED_PARAMS: [ 'provider', 'type' ]
+}
+
+function createLoggerKeyword (opts) {
+  const { provider, type, category } = opts
+  let keyword = `${provider}_${type}`
+  if (category) keyword += `_${category}`
+  return keyword
+}
+
+function createFilter (opts) {
+  const { provider, type, category } = opts
+  let filter = `provider:${provider} AND type:${type}`
+  if (category) filter += ` AND category:${category}`
+  return filter
 }
 
 function createWorker (opts) {
   checkRequiredParams(opts, CONST.REQUIRED_PARAMS)
-  const { provider, type, category } = opts
-  const loggerKeyword = opts.loggerKeyword = `${provider}_${type}_${category}`
+  const { provider } = opts
   const hosts = CONST.CHECK_HOSTS[provider]
-  const log = createLogger({
-    keyword: loggerKeyword,
+  const log = opts.log = createLogger({
+    keyword: createLoggerKeyword(opts),
     diff: true
   })
   const processExit = createProcessExit(log)
-  const worker = providers[provider]({category, type, loggerKeyword})
+  const worker = providers[provider](opts)
 
-  waterfall([
+  const tasks = [
     partial(isUp, hosts),
     function clean (next) {
       log.info('hosts reachability ✔ (1/3)')
-      const filters = `provider:${provider} AND type:${type} AND category:${category}`
+      const filters = createFilter(opts)
       return db.deleteByQuery('', {filters}, next)
     },
     function insert (next) {
       log.info('cleaned old instances ✔ (2/3)')
       return worker(next)
     }
-  ], function (err, stats) {
+  ]
+
+  waterfall(tasks, function (err, stats) {
     if (err) return processExit(err)
     log.debug('stats', stats)
     log.info('inserted new instances ✔ (3/3)')
