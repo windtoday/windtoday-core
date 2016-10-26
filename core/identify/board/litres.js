8f09cd67d002eeaefa1897ad71592f33e9b42219
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-26 10:27:53 +0200

    Fix var scope

--- core/identify/board/litres.js
+++ core/identify/board/litres.js
@@ -2,12 +2,11 @@
 
 const { toNumber, replace, first } = require('lodash')
 
-var REGEX_BOARD_LITRES_WITH_SUFFIX = /[0-9]{2,3}[ ]?l/
-
-var REGEX_BOARD_LITRES_SUFFIX = /l/
+const REGEX_BOARD_LITRES_WITH_SUFFIX = /[0-9]{2,3}[ ]?l/
+const REGEX_BOARD_LITRES_SUFFIX = /l/
 
 function litres (str) {
-  var litres = first(str.match(REGEX_BOARD_LITRES_WITH_SUFFIX))
+  let litres = first(str.match(REGEX_BOARD_LITRES_WITH_SUFFIX))
   if (!litres) return
 
   litres = replace(litres, REGEX_BOARD_LITRES_SUFFIX, '')
