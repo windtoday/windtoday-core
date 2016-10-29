74b92003afaa676c9dc158016d1f2152b949e83a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-29 11:51:02 +0200

    Silent logs on testing

--- core/log/index.js
+++ core/log/index.js
@@ -1,11 +1,18 @@
 'use strict'
 
-const { isString } = require('lodash')
+const { isString, assign } = require('lodash')
 const createLogger = require('acho')
+
+const isTest = process.env.NODE_ENV === 'test'
 
 function log (opts) {
   if (isString(opts)) opts = { keyword: opts }
-  return createLogger(opts)
+
+  const loggerOpts = assign({}, opts, {
+    level: isTest && 'muted'
+  })
+
+  return createLogger(loggerOpts)
 }
 
 module.exports = log
