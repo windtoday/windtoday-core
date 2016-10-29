7384fdc3e7d8886631015c922bf1e4691bbfd377
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-29 13:49:41 +0200

    Remove deserializer

--- core/db/deserializer.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const unflatten = require('unflatten')
-
-function deserializer (obj) {
-  return unflatten(obj)
-}
-
-module.exports = deserializer
--- test/db_spec/index.js
+++ test/db_spec/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const serializer = require('../../core/db/serializer')
-const deserializer = require('../../core/db/deserializer')
 
 describe('db', function () {
   it('serializer', function () {
@@ -14,22 +13,6 @@ describe('db', function () {
     serializer(fixture).should.be.eql({
       category: 'boards',
       'boards.size': 132,
-      brand: 'F2',
-      model: 'Stoke'
-    })
-  })
-
-  it('deserializer', function () {
-    const fixture = {
-      category: 'boards',
-      'boards.size': 132,
-      brand: 'F2',
-      model: 'Stoke'
-    }
-
-    deserializer(fixture).should.be.eql({
-      category: 'boards',
-      boards: { size: 132 },
       brand: 'F2',
       model: 'Stoke'
     })
