80800c67362385c3470ee81a5d9a8cdcf4063f97
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-06 12:31:13 +0100

    Refactor identify category

--- test/identify_spec/category.js
+++ test/identify_spec/category.js
@@ -3,106 +3,88 @@
 const category = require('../../core/identify/category')
 
 describe('identify » category', function () {
-  describe('fins', function () {
-    describe('keywords', function () {
-      describe('truthy', function () {
-        [
-          'aleta',
-          'aleron',
-          'fin',
-          'quilla'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.fins(keyword).should.be.true()
-            category(keyword).should.be.equal('fins')
-          })
+  describe('keywords', function () {
+    describe('fins', function () {
+      [
+        'aleta',
+        'aletas',
+        'aleron',
+        'fin',
+        'fins',
+        'quilla',
+        'quillas'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.fins(keyword).should.be.true()
+          category(keyword).should.be.equal('fins')
         })
       })
     })
-  })
 
-  describe('masts', function () {
-    describe('keywords', function () {
-      describe('truthy', function () {
-        [
-          'mástil',
-          'mastil',
-          'mast'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.masts(keyword).should.be.true()
-            category(keyword).should.be.equal('masts')
-          })
+    describe('masts', function () {
+      [
+        'mástil',
+        'mástiles',
+        'mastil',
+        'mastiles',
+        'mast'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.masts(keyword).should.be.true()
+          category(keyword).should.be.equal('masts')
         })
       })
     })
-  })
 
-  describe('booms', function () {
-    describe('keywords', function () {
-      describe('truthy', function () {
-        [
-          'boom',
-          'botavara'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.booms(keyword).should.be.true()
-            category(keyword).should.be.equal('booms')
-          })
+    describe('booms', function () {
+      [
+        'boom',
+        'booms',
+        'botavara',
+        'botavaras'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.booms(keyword).should.be.true()
+          category(keyword).should.be.equal('booms')
         })
       })
     })
-  })
 
-  describe('sails', function () {
-    describe('keywords', function () {
-      describe('truthy', function () {
-        [
-          'vela',
-          'sail'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.sails(keyword).should.be.true()
-            category(keyword).should.be.equal('sails')
-          })
-        })
-      })
-
-      describe('falsy', function () {
-        [
-          'manivela'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.sails(keyword).should.be.false()
-            category(keyword).should.be.equal('others')
-          })
+    describe('sails', function () {
+      [
+        'vela',
+        'velas',
+        'sail'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.sails(keyword).should.be.true()
+          category(keyword).should.be.equal('sails')
         })
       })
     })
-  })
 
-  describe('boards', function () {
-    describe('keywords', function () {
-      describe('truthy', function () {
-        [
-          'board',
-          'tabla'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.boards(keyword).should.be.true()
-            category(keyword).should.be.equal('boards')
-          })
+    describe('boards', function () {
+      [
+        'board',
+        'boards',
+        'tabla',
+        'tablas'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.boards(keyword).should.be.true()
+          category(keyword).should.be.equal('boards')
         })
       })
+    })
 
-      describe('falsy', function () {
-        [
-          'starboard'
-        ].forEach(function (keyword) {
-          it(keyword, function () {
-            category.boards(keyword).should.be.false()
-            category(keyword).should.be.equal('others')
-          })
+    describe('others', function () {
+      [
+        'manivela',
+        'starboard'
+      ].forEach(function (keyword) {
+        it(keyword, function () {
+          category.sails(keyword).should.be.false()
+          category(keyword).should.be.equal('others')
         })
       })
     })
