24bbe45ea127cb051b745185f9388ea0c93e1b19
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-08 21:24:53 +0100

    Update year for support clean matching

--- core/identify/year/index.js
+++ core/identify/year/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const { toNumber, first } = require('lodash')
+const { toNumber } = require('lodash')
+const strmatch = require('str-match')
+// TODO: pass options to strmatch to decide how to apply the clean regex
 
 /**
  * Detect year of 4 digits in [2000-2999] range.
@@ -8,9 +10,15 @@ const { toNumber, first } = require('lodash')
  */
 const REGEX_YEAR = /2[0-9]{3}/
 
+function response (year, output) {
+  return { data: {year}, output }
+}
+
 function year (str) {
-  var year = first(str.match(REGEX_YEAR))
-  return year && toNumber(year)
+  const year = strmatch(str, REGEX_YEAR)
+  if (!year.test) return response(undefined, str)
+  year.match = toNumber(year.match)
+  return response(year.match, year.output)
 }
 
 year.regex = REGEX_YEAR
--- test/identify_spec/year.js
+++ test/identify_spec/year.js
@@ -1,18 +1,23 @@
 'use strict'
 
-const should = require('should')
 const year = require('../../core/identify/year')
+const { get } = require('lodash')
+const should = require('should')
 
 describe('identify » year', function () {
   it('not detect', function () {
     [
       ''
     ].forEach(function (str) {
-      should(year(str)).be.undefined()
+      const {data, output} = year(str)
+      should(get(data, 'year')).be.undefined()
+      output.includes('2011').should.be.false()
     })
   })
 
   it('detect', function () {
-    year('Vendo Loft Blade 7,8 2011 - 200€').should.be.equal(2011)
+    const {data, output} = year('Vendo Loft Blade 7,8 2011 200€')
+    should(get(data, 'year')).be.equal(2011)
+    output.includes('2011').should.be.false()
   })
 })
