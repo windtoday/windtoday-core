58e8c1f3bd3dde017a8f5985d6be51f2bfbc20a5
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-13 19:27:31 +0100

    pipeline → flow

--- core/identify/autodetection/index.js
+++ core/identify/autodetection/index.js
@@ -2,14 +2,14 @@
 
 const getIdentifiers = require('./identifiers')
 const getCategories = require('../category')
-const pipeline = require('./pipeline')
 const { assign } = require('lodash')
+const flow = require('./flow')
 
 function createAutodetection (loggerKeyword) {
   function autodetect (str) {
     const {data, output} = getCategories(str)
     const identifiers = getIdentifiers(data)
-    const detection = pipeline(output, loggerKeyword, identifiers)
+    const detection = flow(output, loggerKeyword, identifiers)
 
     detection.data.category = data
     assign(detection.data, {category: data})
