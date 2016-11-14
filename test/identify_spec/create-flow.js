be24daed9f92cbe1056ee9435142d9b51d284569
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-14 22:01:53 +0100

    Extend test

--- test/identify_spec/create-flow.js
+++ test/identify_spec/create-flow.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const createFlow = require('../../core/identify/create-flow')
+const log = require('../../core/log')
 const price = require('../../core/identify/price')
 const year = require('../../core/identify/year')
+const mast = require('../../core/identify/mast')(log('test'))
+const sail = require('../../core/identify/sail')(log('test'))
 
 describe('identify » create flow', function () {
   it('price and year', function () {
@@ -18,5 +21,21 @@ describe('identify » create flow', function () {
     data.should.have.property('price', 260)
     data.should.have.property('year', 2011)
     output.should.be.equal('Vendo vela  precio ')
+  })
+
+  it('sail and mast', function () {
+    const flow = createFlow({
+      identifiers: [
+        {fn: sail, name: 'sail'},
+        {fn: mast, name: 'mast'}
+      ]
+    })
+
+    const str = 'Neilpryde Hellcat 6,7. Mastil Rdm 430 Y Botavara North'
+    const {data, output} = flow(str)
+
+    data.should.have.property('sail.size', 6.7)
+    data.should.have.property('mast.size', 430)
+    output.should.be.equal('  . Mastil   Y Botavara North')
   })
 })
