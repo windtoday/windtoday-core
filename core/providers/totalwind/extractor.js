d1ad8a2fc67a80a98f8016ab96d21995df071090
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-11-25 17:57:35 +0100

    Fix log path

--- core/providers/totalwind/extractor.js
+++ core/providers/totalwind/extractor.js
@@ -11,8 +11,8 @@ function createExtractor (log, path) {
 
 function specificExtractor (opts) {
   const { path, log } = opts
-  const loggerKeyword = log.keyword
-  const extractorLogger = createLogger(`${loggerKeyword}_unidentify`)
+  let loggerKeyword = log.keyword
+  const extractorLogger = createLogger(`${loggerKeyword}_${path}_unidentify`)
 
   switch (path) {
     case 'sails':
@@ -24,6 +24,7 @@ function specificExtractor (opts) {
     case 'booms':
       return createExtractor(extractorLogger, 'boom')
     default:
+      loggerKeyword = `${loggerKeyword}_${path}`
       // TODO: Ideally, not necessary use autodetection becase at this point
       // we don't need to process as sails or boards.
       // Create a pipeline for a very closed detection.
