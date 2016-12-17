6477ec9f7f77d92372b79231bb9b65e2789f99d3
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-12-17 03:46:03 +0100

    Fix typo

--- core/worker/process-exit.js
+++ core/worker/process-exit.js
@@ -7,9 +7,8 @@ function factory (log) {
     if (!err) return process.exit()
 
     const message = get(err, 'message', err)
-    const code = get(err, code, 1)
+    const code = get(err, 'code', 1)
 
-    log.error({reason: message})
     return process.exit(code)
   }
 
