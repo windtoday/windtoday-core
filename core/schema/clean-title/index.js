81c48a3adc9fc1e6cc58f98afa258f7746a4ac03
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-12-18 11:58:23 +0100

    Fix cleanup price from title

--- core/schema/clean-title/index.js
+++ core/schema/clean-title/index.js
@@ -1,18 +1,17 @@
 'use strict'
 
-const { flow } = require('lodash')
-const titleize = require('titleize')
-const cleanWhiteSpaces = require('condense-whitespace')
-
 const REGEX_WORDS = RegExp(require('./words.json').join('|'), 'ig')
-const REGEX_PRICE = RegExp(require('../../identify/price').regex, 'g')
+const cleanWhiteSpaces = require('condense-whitespace')
+const price = require('../../identify/price')
+const titleize = require('titleize')
+const { flow } = require('lodash')
 
 function cleanWords (str) {
   return str.replace(REGEX_WORDS, '')
 }
 
 function cleanPrice (str) {
-  return str.replace(REGEX_PRICE, '')
+  return price(str).output
 }
 
 module.exports = flow([
