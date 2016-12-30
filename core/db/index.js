6d728ae664939e1edc4543734b77ec0e512a4ccf
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-12-31 00:40:05 +0100

    Remove legacy code

--- core/db/index.js
+++ core/db/index.js
@@ -2,17 +2,9 @@
 
 const algoliasearch = require('algoliasearch')
 const CONFIG = require('config').algoliasearch
-const { each } = require('async')
-
 const appId = process.env[CONFIG.app_id]
 const apiKey = process.env[CONFIG.api_key]
 const client = algoliasearch(appId, apiKey)
 const index = client.initIndex(CONFIG.index)
-
-index.insertAll = function insertAll (providers, cb) {
-  each(providers, function (provider, next) {
-    return provider(next)
-  }, cb)
-}
 
 module.exports = index
