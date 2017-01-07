73d9223d4b908731382b0bed66cb990b1edd2531
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-07 01:26:29 +0100

    Sort document before insert/compare

--- core/db/index.js
+++ core/db/index.js
@@ -1,30 +1,36 @@
 'use strict'
 
+const { sortBy, difference, map, size, mapValues } = require('lodash')
 const { waterfall, parallel } = require('async')
-const { concat, map, size, mapValues } = require('lodash')
 
 const search = require('./search')
 const state = require('./state')
 
+const CONST = {
+  DIFF_ID: 'title'
+}
+
 function add (opts, cb) {
   const {key, docs} = opts
+  const value = sortBy(docs, CONST.DIFF_ID)
 
   const tasks = [
     function compare (next) {
-      return state.compare({key, value: docs, id: 'title'}, next)
+      return state.compare({key, value, id: CONST.DIFF_ID}, next)
     },
     function update (diff, next) {
+      const stats = mapValues(diff, size)
+
       const subTasks = [
         (done) => search.addObjects(diff.added, done),
-        (done) => search.deleteObjects(map(diff.removed, 'objectID'), done)
+        (done) => search.deleteObjects(map(diff.removed, CONST.DIFF_ID), done)
       ]
 
-      const stats = mapValues(diff, size)
-      const newState = concat(diff.added, diff.common)
+      const newState = sortBy(difference(value, diff.removed), CONST.DIFF_ID)
       return parallel(subTasks, (err) => next(err, newState, stats))
     },
-    function saveState (newState, stats, next) {
-      return state.set({key, value: newState}, (err) => next(err, stats))
+    function saveState (value, stats, next) {
+      return state.set({key, value}, (err) => next(err, stats))
     }
   ]
 
