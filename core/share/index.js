0aab83fedf4f82d711236fe2eaafee01534d7a80
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-08 23:58:57 +0100

    Disable temporaly sharing because it's buggy

--- core/share/index.js
+++ core/share/index.js
@@ -3,17 +3,17 @@
 const bufferapp = require('buffer-node')
 const CONFIG = require('config').buffer
 const compose = require('./compose')
-const {each} = require('async')
+const {each} = require('async') // eslint-disable-line
 const {get} = require('lodash')
 
 const accessToken = get(global, CONFIG.access_token)
 const client = bufferapp(accessToken)
 
-function update (doc, cb) {
+function update (doc, cb) { // eslint-disable-line
   const text = compose(doc)
   return client.updates.create(text, CONFIG.accounts).nodeify(cb)
 }
 
-const updateDocs = (docs, cb) => each(docs, update, cb)
+const updateDocs = (docs, cb) => cb()
 
 module.exports = updateDocs
