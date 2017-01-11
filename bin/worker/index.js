b08c2ac9a00b5f8d077b7f70ed7543f20b2ff7a6
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-11 17:46:51 +0100

    Remove unnecessary

--- bin/worker/index.js
+++ bin/worker/index.js
@@ -4,12 +4,6 @@
 
 const createWorker = require('../../core/worker')
 const meow = require('meow')
-const path = require('path')
-const fs = require('fs')
 
-const cli = meow({
-  pkg: '../../package.json',
-  help: fs.readFileSync(path.join(__dirname, '/help.txt'), 'utf8')
-})
-
+const cli = meow()
 createWorker(cli.flags)
