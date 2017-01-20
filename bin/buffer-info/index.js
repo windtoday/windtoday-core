463de9c8b5a0d04c557514d4710cd01715bbefa3
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-20 10:44:54 +0100

    Add buffer script

---
+++ bin/buffer-info/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const bufferapp = require('buffer-node')
+const CONFIG = require('config').buffer
+const {get} = require('lodash')
+const accessToken = get(global, CONFIG.access_token)
+const buffer = bufferapp(accessToken)
+
+buffer.profiles.get().nodeify(function (err, response) {
+  if (err) throw err
+  response.forEach(function (item) {
+    const {service, id} = item
+    console.log({service, id})
+  })
+})
