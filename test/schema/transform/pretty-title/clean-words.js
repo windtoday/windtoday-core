ffdabcf80ce5ab10aabdce4b46c1350352530845
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-26 20:50:25 +0100

    Improve pretty title

--- test/schema/transform/pretty-title/clean-words.js
+++ test/schema/transform/pretty-title/clean-words.js
@@ -4,7 +4,7 @@ require('should')
 const cleanWords = require('../../../../core/schema/transform/pretty-title/clean-words')
 
 describe('schema » transform » pretty title » clean words', function () {
-  const input = 'Vendo Mistral Syncro - 280€'
+  const input = 'Vendo Mistral Syncro - 280€ .-'
   const expected = 'Mistral Syncro 280€'
 
   it(`${input} → ${expected}`, function () {
