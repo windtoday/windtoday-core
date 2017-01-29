a8e69753bf437c8985395a38a5bfd4c068979bc5
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-29 17:56:10 +0100

    Update interface

--- bin/bootstrap/create-bootstrap.js
+++ bin/bootstrap/create-bootstrap.js
@@ -1,14 +1,24 @@
 'use strict'
 
-const createWorker = require('../../core/worker')
 const {assign} = require('lodash')
 
+const createLoggerKeyword = require('../../core/util/create-logger-keyword')
+const createWorker = require('../../core/worker')
+const createLogger = require('../../core/log')
+
 function createBootstrap (flags) {
+  const log = createLogger({
+    keyword: createLoggerKeyword(flags),
+    diff: true
+  })
+
   function bootstrap (cb) {
-    const opts = assign(flags, {share: false})
+    const opts = assign(flags, {log, share: false})
     const worker = createWorker(opts)
     return worker(cb)
   }
+
+  bootstrap.log = log
 
   return bootstrap
 }
--- bin/bootstrap/index.js
+++ bin/bootstrap/index.js
@@ -1,22 +1,16 @@
 'use strict'
 
-const {assign} = require('lodash')
 const meow = require('meow')
 
-const createLoggerKeyword = require('../../core/util/create-logger-keyword')
 const createProcessExit = require('../../core/util/create-process-exit')
 const createBootsrap = require('./create-bootstrap')
-const createLogger = require('../../core/log')
 
 const cli = meow()
 const {flags} = cli
 
-const log = createLogger({
-  keyword: createLoggerKeyword(flags),
-  diff: true
-})
+const bootstrap = createBootsrap(flags)
+const {log} = bootstrap
 
-const bootstrap = createBootsrap(assign({log}, flags))
 const processExit = createProcessExit(log)
 
 bootstrap(processExit)
