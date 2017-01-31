30981bfc77a9d536d4d8c550337f1b01c1e270e5
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-01-31 14:45:44 +0100

    Fix test

--- test/schema/transform/pretty-title/index.js
+++ test/schema/transform/pretty-title/index.js
@@ -4,14 +4,8 @@ require('should')
 const prettyTitle = require('../../../../core/schema/transform/pretty-title')
 
 describe('schema » transform » pretty title', function () {
-  it('titleize', function () {
-    const input = 'Vendo mistral Syncro - 280€'
-    const output = prettyTitle(input)
-    output.should.be.equal('Mistral Syncro')
-  })
-
   it('whitespaces', function () {
-    const input = '  Vendo  mistral  Syncro - 280€   '
+    const input = '  Vendo  Mistral  Syncro - 280€   '
     const output = prettyTitle(input)
     output.should.be.equal('Mistral Syncro')
   })
