dcb5c7d1fddb91988481383a67f6c887116dc475
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-12 18:43:40 +0100

    Update remove script

--- bin/remove/create-params.js
+++ bin/remove/create-params.js
@@ -2,10 +2,9 @@
 
 module.exports = function createParams (opts) {
   if (!opts.provider) throw TypeError('Need to provide the provider.')
-  if (!opts.seller) throw TypeError('Need to provide the seller.')
   const { provider, seller, path } = opts
-
-  let filters = `provider:${provider} AND seller:${seller}`
+  let filters = `provider:${provider}`
+  if (seller) filters += ` AND seller:${seller}`
   if (path) filters += ` AND path:${path}`
   return {filters}
 }
