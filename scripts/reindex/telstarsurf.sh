bb36cee3244d27cc24fb8cc42df95dd3bcc7933d
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-12 18:38:54 +0100

    Fix scripts

--- scripts/reindex/telstarsurf.sh
+++ scripts/reindex/telstarsurf.sh
@@ -3,8 +3,26 @@
 export NODE_ENV=production
 
 # telstarsurf
-(node bin/reindex --provider=telstarsurf --seller=all --path=sails)
-(node bin/reindex --provider=telstarsurf --seller=all --path=boards --no-checkHosts)
-(node bin/reindex --provider=telstarsurf --seller=all --path=masts --no-checkHosts)
-(node bin/reindex --provider=telstarsurf --seller=all --path=booms --no-checkHosts)
-(node bin/reindex --provider=telstarsurf --seller=all --path=fins --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=used --path=sails)
+(node bin/reindex --provider=telstarsurf --seller=wave --path=sails --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freestyle --path=sails --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freeride --path=sails --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freerace --path=sails --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=slalom --path=sails --no-checkHosts)
+
+(node bin/reindex --provider=telstarsurf --seller=used --path=boards --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=wave --path=boards --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freestyle --path=boards --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freeride --path=boards --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freerace --path=boards --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=slalom --path=boards --no-checkHosts)
+
+(node bin/reindex --provider=telstarsurf --seller=rdm --path=masts --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=sdm --path=masts --no-checkHosts)
+
+(node bin/reindex --provider=telstarsurf --seller=aluminum --path=booms --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=carbon --path=booms --no-checkHosts)
+
+(node bin/reindex --provider=telstarsurf --seller=wave --path=fins --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=freeride --path=fins --no-checkHosts)
+(node bin/reindex --provider=telstarsurf --seller=slalom --path=fins --no-checkHosts)
