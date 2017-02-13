03ceeb015b786a2e7273a88c6ce62ad142e3a8a2
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-13 13:53:20 +0100

    Refactor

--- core/db/index.js
+++ core/db/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { chain, map, size, mapValues } = require('lodash')
+const { chain, map, size, mapValues, concat } = require('lodash')
 const { waterfall, parallel } = require('async')
 
 const search = require('./search')
@@ -11,34 +11,31 @@ const CONST = {
   DIFFERENCE_IDS: ['title', 'price']
 }
 
-function add (opts, cb) {
-  const {key, docs} = opts
-
-  const value = chain(docs)
+const getSnapshot = (collection) => {
+  return chain(collection)
   .uniqBy(CONST.UNIQUE_ID)
   .sortBy(CONST.UNIQUE_ID)
   .value()
+}
+
+function add (opts, cb) {
+  const {key, docs} = opts
 
   const tasks = [
     function compare (next) {
       const ids = CONST.DIFFERENCE_IDS
+      const value = getSnapshot(docs)
       return state.compare({key, value, ids}, next)
     },
     function update (diff, next) {
       const stats = mapValues(diff, size)
       const {added, common, removed} = diff
-
+      const newDocs = concat(common, added)
+      const newState = getSnapshot(newDocs)
       const subTasks = [
         (done) => search.addObjects(added, done),
         (done) => search.deleteObjects(map(removed, CONST.UNIQUE_ID), done)
       ]
-
-      const newState = chain(common)
-        .concat(added)
-        .uniqBy(CONST.UNIQUE_ID)
-        .sortBy(CONST.UNIQUE_ID)
-        .value()
-
       return parallel(subTasks, (err) => next(err, newState, added, stats))
     },
     function saveState (value, added, stats, next) {
