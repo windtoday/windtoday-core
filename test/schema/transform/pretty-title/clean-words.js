8484c0fe9cdc52b84b1ea63f46426580fbd0df33
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-14 21:49:55 +0100

    Improve clean words

--- test/schema/transform/pretty-title/clean-words.js
+++ test/schema/transform/pretty-title/clean-words.js
@@ -1,14 +1,48 @@
 'use strict'
 
-require('should')
+const should = require('should')
 const cleanWords = require('../../../../core/schema/transform/pretty-title/clean-words')
 
 describe('schema » transform » pretty title » clean words', function () {
-  const input = 'Vendo Mistral Syncro - 280€ .-'
-  const expected = 'Mistral Syncro 280€'
-
-  it(`${input} → ${expected}`, function () {
+  it('vendo', function () {
+    const input = 'Vendo Mistral Syncro'
+    const expected = 'Mistral Syncro'
     const output = cleanWords(input)
-    output.should.be.equal(expected)
+    should(output).be.equal(expected)
+  })
+
+  it('-', function () {
+    const input = 'Mistral Syncro - €280'
+    const expected = 'Mistral Syncro €280'
+    const output = cleanWords(input)
+    should(output).be.equal(expected)
+  })
+
+  it('.-', function () {
+    const input = 'Mistral Syncro .- €280'
+    const expected = 'Mistral Syncro €280'
+    const output = cleanWords(input)
+    should(output).be.equal(expected)
+  })
+
+  it('----', function () {
+    const input = 'SIMMER -------- 5.7 ---- 150€'
+    const expected = 'SIMMER 5.7 150€'
+    const output = cleanWords(input)
+    should(output).be.equal(expected)
+  })
+
+  it('de', function () {
+    const input = 'Gaastra Nitro 3 de 9.8'
+    const expected = 'Gaastra Nitro 3 9.8'
+    const output = cleanWords(input)
+    should(output).be.equal(expected)
+  })
+
+  it('del', function () {
+    const input = 'Gaastra Nitro 3 del 2011'
+    const expected = 'Gaastra Nitro 3 2011'
+    const output = cleanWords(input)
+    should(output).be.equal(expected)
   })
 })
