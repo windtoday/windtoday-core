a28c02bde10a43b10d349622afe231e68c5214b5
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-17 20:51:54 +0100

    Fix remove object by id

--- core/db/index.js
+++ core/db/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { map, size, mapValues, concat } = require('lodash')
-const { waterfall, parallel } = require('async')
+const { waterfall, series } = require('async')
 
 const createSnapshot = require('../util/create-snapshot')
 const search = require('./search')
@@ -13,7 +13,21 @@ const CONST = {
     uniqId: ['url', 'title']
   },
   DIFF_IDS: ['title', 'url', 'price'],
-  REMOVE_ID: 'objectID'
+  OBJECT_ID: 'objectID'
+}
+
+function mapObjectID (objects, objectIDs) {
+  return map(objects, function (item, index) {
+    item[CONST.OBJECT_ID] = objectIDs[index]
+    return item
+  })
+}
+
+function createTransaction (objectIDs, added) {
+  return [
+    (done) => search.deleteObjects(objectIDs, done),
+    (done) => search.addObjects(added, done)
+  ]
 }
 
 const getSnapshot = createSnapshot(CONST.SNAPSHOT_OPTS)
@@ -28,16 +42,21 @@ function add (opts, cb) {
       return state.compare({key, value, ids}, next)
     },
     function update (diff, next) {
-      const stats = mapValues(diff, size)
       const {added, common, removed} = diff
-      const newDocs = concat(common, added)
-      const newState = getSnapshot(newDocs)
-      const removedIds = map(removed, CONST.REMOVE_ID)
-      const subTasks = [
-        (done) => search.deleteObjects(removedIds, done),
-        (done) => search.addObjects(added, done)
-      ]
-      return parallel(subTasks, (err) => next(err, newState, added, stats))
+      const objectIDs = map(removed, CONST.OBJECT_ID)
+      const transaction = createTransaction(objectIDs, added)
+
+      series(transaction, function (err, results) {
+        if (err) return next(err)
+
+        const [, content] = results
+        const {objectIDs} = content
+        const data = mapObjectID(added, objectIDs)
+        const stats = mapValues(diff, size)
+        const newDocs = concat(common, data)
+        const newState = getSnapshot(newDocs)
+        return next(err, newState, data, stats)
+      })
     },
     function saveState (value, added, stats, next) {
       return state.set({key, value}, (err) => next(err, added, stats))
