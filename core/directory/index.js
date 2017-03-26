e959a9c2dbe48059f682d418aef6044adac4975a
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-03-26 15:00:30 +0200

    Link accesories directory

--- core/directory/index.js
+++ core/directory/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+  accesories: require('windtoday-windsurf-accesories'),
   boards: require('windtoday-windsurf-boards'),
   fins: require('windtoday-windsurf-fins'),
   sails: require('windtoday-windsurf-sails')
