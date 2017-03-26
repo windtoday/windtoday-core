46a106e2de3cdf64b7bbe7d32cbfd4685d7c03f9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-03-26 14:28:53 +0200

    Test refactor

--- test/identify/create-flow.js
+++ test/identify/create-flow.js
@@ -1,10 +1,10 @@
 'use strict'
 
+const should = require('should')
+
 const createFlow = require('../../core/identify/create-flow')
-const price = require('../../core/identify/price')
-const year = require('../../core/identify/year')
-const mast = require('../../core/identify/mast')
-const sail = require('../../core/identify/sail')
+const identify = require('../../core/identify')
+const {price, year, mast, sail} = identify
 const createLogger = require('../../core/log')
 
 describe('identify » create flow', function () {
@@ -16,9 +16,9 @@ describe('identify » create flow', function () {
 
     const {data, output} = flow('Vendo vela 2011 precio 260€')
 
-    data.should.have.property('price', 260)
-    data.should.have.property('year', 2011)
-    output.should.be.equal('Vendo vela  precio ')
+    should(data).have.property('price', 260)
+    should(data).have.property('year', 2011)
+    should(output).be.equal('Vendo vela  precio ')
   })
 
   it('sail and mast', function () {
@@ -29,10 +29,10 @@ describe('identify » create flow', function () {
 
     const str = 'Neilpryde Hellcat 6,7. Mastil Rdm 430 Y Botavara North'
     const {data, output} = flow(str)
-    data.should.have.property('sail size', 6.7)
-    data.should.have.property('mast size', 430)
-    data.category.should.be.eql(['sails', 'masts'])
-    data.brand.should.be.eql(['Neilpryde', 'North'])
-    output.should.be.equal('  . Mastil   Y Botavara ')
+    should(data).have.property('sail size', 6.7)
+    should(data).have.property('mast size', 430)
+    should(data.category).be.eql(['sails', 'masts'])
+    should(data.brand).be.eql(['Neilpryde', 'North'])
+    should(output).be.equal('  . Mastil   Y Botavara ')
   })
 })
