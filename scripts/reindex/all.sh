ecc6bfadd957fade74d2cf4c905d77bab9d2677c
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-04-08 17:10:55 +0200

    Update

--- scripts/reindex/all.sh
+++ scripts/reindex/all.sh
@@ -4,6 +4,7 @@ export NODE_ENV=production
 CURRENT_DIR=$(dirname $0)
 
 . $CURRENT_DIR/bellini.sh
+. $CURRENT_DIR/bigsurfshop.sh
 . $CURRENT_DIR/easysurfshop.sh
 . $CURRENT_DIR/ozutarifa.sh
 . $CURRENT_DIR/surfkeppler.sh
