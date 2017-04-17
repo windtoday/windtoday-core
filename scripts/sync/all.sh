508992f9740d35070224a4649881869d6ab83759
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-04-17 14:38:05 +0200

    Add sync all script

---
+++ scripts/sync/all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export NODE_ENV=production
+CURRENT_DIR=$(dirname $0)
+
+. $CURRENT_DIR/bellini.sh
+. $CURRENT_DIR/bigsurfshop.sh
+. $CURRENT_DIR/easysurfshop.sh
+. $CURRENT_DIR/lpwind.sh
+. $CURRENT_DIR/ozutarifa.sh
+. $CURRENT_DIR/surfkeppler.sh
+. $CURRENT_DIR/telstarsurf.sh
+. $CURRENT_DIR/totalwind.sh
