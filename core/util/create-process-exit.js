8f5cd296729a1429c300c8a673df332131b34125
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-01 00:51:15 +0200

    Add rollbar

--- core/util/create-process-exit.js
+++ core/util/create-process-exit.js
@@ -1,6 +1,13 @@
 'use strict'
 
 const cleanStack = require('clean-stack')
+const CONFIG = require('config').rollbar
+const Rollbar = require('rollbar')
+const {get} = require('lodash')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const accessToken = get(global, CONFIG)
+const rollbar = new Rollbar(accessToken)
 
 function createProcessExit (log) {
   function processExit (err) {
@@ -10,6 +17,7 @@ function createProcessExit (log) {
 
     log.fatal(message || err)
     if (stack) log.fatal(cleanStack(err.stack))
+    if (isProduction) rollbar.error(err)
 
     return process.exit(code)
   }
