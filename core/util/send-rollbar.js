9920ac917e05b19801bca43a76d2310fd93b9ffc
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-01 10:29:56 +0200

    Update rollbar environment

--- core/util/send-rollbar.js
+++ core/util/send-rollbar.js
@@ -4,11 +4,13 @@ const CONFIG = require('config').rollbar
 const Rollbar = require('rollbar')
 const {get} = require('lodash')
 
-const isProduction = process.env.NODE_ENV === 'production'
+const {NODE_ENV = 'development'} = process.env
+const isProduction = NODE_ENV === 'production'
 const accessToken = get(global, CONFIG)
 
 const rollbar = new Rollbar({
   accessToken: accessToken,
+  environment: NODE_ENV,
   handleUncaughtExceptions: true,
   handleUnhandledRejections: true
 })
