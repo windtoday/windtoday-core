9c291b4484e224fb8247c6221dae8da21f299574
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-01 12:26:26 +0200

    Do not send validation errors

--- core/provider/create/create-add.js
+++ core/provider/create/create-add.js
@@ -2,7 +2,6 @@
 
 const {assign} = require('lodash')
 
-const sendRollbar = require('../../util/send-rollbar')
 const schema = require('../../schema')
 
 function createAdd (opts) {
@@ -13,14 +12,11 @@ function createAdd (opts) {
       if (!validationError) {
         acc.push(docValidated)
       } else {
-        const errMessage = 'schema'
-        const errPayload = {
+        log.error(schema, {
           key: validationError.key,
           value: validationError.value,
           title: doc.title
-        }
-        sendRollbar('error', errMessage, errPayload)
-        log.error(errMessage, errPayload)
+        })
       }
 
       return cb(null, acc)
