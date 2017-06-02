b1d52d1438eb848732a14f7b81abccfde20ed867
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-06-02 19:30:37 +0200

    Add ability to use specific directory opts

--- core/directory/create-flow.js
+++ core/directory/create-flow.js
@@ -4,9 +4,9 @@ const { mergeWith, reduce } = require('lodash')
 const mergeProps = require('../util/merge-props')
 
 function createFlow (directories) {
-  function flow (str) {
+  function flow (str, opts) {
     return reduce(directories, function (acc, directory) {
-      const {data, output} = directory(acc.output)
+      const {data, output} = directory(acc.output, opts)
       acc.output = output
       acc.data = mergeWith(acc.data, data, mergeProps)
       return acc
