29bbe87c153b899600b36499e9cff033cce78d03
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-23 08:46:34 +0200

    Add support for filter shareables by date

--- core/share/get-shareables.js
+++ core/share/get-shareables.js
@@ -2,6 +2,7 @@
 
 const {includes, filter, overEvery} = require('lodash')
 const {premium_providers: premiumProviders} = require('config')
+const isToday = require('date-fns/is_today')
 
 const MIN_PRICE_SCORE = 90
 
@@ -9,6 +10,12 @@ const isDeal = ({priceScore}) => priceScore >= MIN_PRICE_SCORE
 
 const isPremiumShop = item => includes(premiumProviders, item.provider)
 
-const conditions = overEvery([isPremiumShop, isDeal])
+const isRecent = item => isToday(item.timestamp)
+
+const conditions = overEvery([
+  isRecent,
+  isPremiumShop,
+  isDeal
+])
 
 module.exports = items => filter(items, conditions)
--- test/share/get-shareables.js
+++ test/share/get-shareables.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const endOfYesterday = require('date-fns/end_of_yesterday')
 const should = require('should')
 
 const getShareables = require('../../core/share/get-shareables')
+
+const now = Date.now()
 
 describe('share » get shareables', function () {
   it('filter non premium shops', function () {
@@ -49,6 +52,86 @@ describe('share » get shareables', function () {
         price: 550,
         year: 2015,
         priceScore: 89,
+        'sail.size': 4.9,
+        timestamp: now
+      },
+      {
+        title: 'Ezzy Taka 4,9 2015',
+        category: ['sails'],
+        provider: 'lpwind',
+        seller: 'particular',
+        link: 'http:/totalwind.net/foro/viewtopic.php?f=49&t=95973',
+        brand: 'Ezzy',
+        model: 'Taka',
+        price: 550,
+        year: 2015,
+        priceScore: 90,
+        'sail.size': 4.9,
+        timestamp: now
+      },
+      {
+        title: 'Ezzy Taka 4,9 2015',
+        category: ['sails'],
+        provider: 'easysurfshop',
+        seller: 'particular',
+        link: 'http:/totalwind.net/foro/viewtopic.php?f=49&t=95973',
+        brand: 'Ezzy',
+        model: 'Taka',
+        price: 260,
+        year: 2015,
+        priceScore: 95,
+        'sail.size': 4.9,
+        timestamp: now
+      }
+    ]
+
+    const shareables = getShareables(items)
+    should(shareables).be.eql([
+      {
+        title: 'Ezzy Taka 4,9 2015',
+        category: ['sails'],
+        provider: 'lpwind',
+        seller: 'particular',
+        link: 'http:/totalwind.net/foro/viewtopic.php?f=49&t=95973',
+        brand: 'Ezzy',
+        model: 'Taka',
+        price: 550,
+        year: 2015,
+        priceScore: 90,
+        'sail.size': 4.9,
+        timestamp: now
+      },
+      {
+        title: 'Ezzy Taka 4,9 2015',
+        category: ['sails'],
+        provider: 'easysurfshop',
+        seller: 'particular',
+        link: 'http:/totalwind.net/foro/viewtopic.php?f=49&t=95973',
+        brand: 'Ezzy',
+        model: 'Taka',
+        price: 260,
+        year: 2015,
+        priceScore: 95,
+        'sail.size': 4.9,
+        timestamp: now
+      }
+    ])
+  })
+
+  it('filter items fetched in the last 24hours', function () {
+    const items = [
+      {
+        title: 'Ezzy Taka 4,9 2015',
+        category: ['sails'],
+        provider: 'bellini',
+        seller: 'particular',
+        link: 'http:/totalwind.net/foro/viewtopic.php?f=49&t=95973',
+        brand: 'Ezzy',
+        model: 'Taka',
+        price: 550,
+        year: 2015,
+        priceScore: 89,
+        timestamp: endOfYesterday(),
         'sail.size': 4.9
       },
       {
@@ -62,6 +145,7 @@ describe('share » get shareables', function () {
         price: 550,
         year: 2015,
         priceScore: 90,
+        timestamp: now,
         'sail.size': 4.9
       },
       {
@@ -75,6 +159,7 @@ describe('share » get shareables', function () {
         price: 260,
         year: 2015,
         priceScore: 95,
+        timestamp: now,
         'sail.size': 4.9
       }
     ]
@@ -92,7 +177,8 @@ describe('share » get shareables', function () {
         price: 550,
         year: 2015,
         priceScore: 90,
-        'sail.size': 4.9
+        'sail.size': 4.9,
+        timestamp: now
       },
       {
         title: 'Ezzy Taka 4,9 2015',
@@ -105,7 +191,8 @@ describe('share » get shareables', function () {
         price: 260,
         year: 2015,
         priceScore: 95,
-        'sail.size': 4.9
+        'sail.size': 4.9,
+        timestamp: now
       }
     ])
   })
