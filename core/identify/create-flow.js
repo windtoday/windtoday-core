22bafef6cd55745a2b18ca1d38b8c69b8fb28def
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-27 23:23:06 +0200

    Refactor

--- core/identify/create-flow.js
+++ core/identify/create-flow.js
@@ -5,21 +5,15 @@ const { mergeWith, reduce } = require('lodash')
 const serializer = require('../schema/serializer')
 const mergeProps = require('../util/merge-props')
 
-function createFlow (identifiers) {
-  function flow (str) {
-    return reduce(
-      identifiers,
-      function (acc, identify) {
-        const { data, output } = identify(acc.output)
-        acc.output = output
-        acc.data = mergeWith(acc.data, serializer(data), mergeProps)
-        return acc
-      },
-      { output: str }
-    )
-  }
-
-  return flow
-}
+const createFlow = identifiers => str => reduce(
+  identifiers,
+  function (acc, identify) {
+    const { data, output } = identify(acc.output)
+    acc.output = output
+    acc.data = mergeWith(acc.data, serializer(data), mergeProps)
+    return acc
+  },
+  { output: str }
+)
 
 module.exports = createFlow
