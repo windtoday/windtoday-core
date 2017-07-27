39350c223e4b6d1afd28c3875fd833100abe655d
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-27 23:35:49 +0200

    Update tests

--- test/identify/boom/type.js
+++ test/identify/boom/type.js
@@ -28,10 +28,10 @@ describe('identify » bom » type', function () {
 
   describe('special vendors', function () {
     ;[
-      {str: 'Neilpryde X9', type: 'carbon'},
-      {str: 'Severne Enigma', type: 'carbon'},
-      {str: 'North Platinum', type: 'carbon'},
-      {str: 'North Gold Series', type: 'carbon'}
+      {str: 'NEILPRYDE X9 BOOM 140 €839', type: 'carbon'},
+      {str: 'SEVERNE SAILS ENIGMA BOOM 140 €879', type: 'carbon'},
+      {str: 'NORTHSAILS PLATINUM RACE 240 €1140', type: 'carbon'},
+      {str: 'NORTHSAILS GOLD SERIES RS 170 €369', type: 'carbon'}
     ].forEach(({str, type}) => {
       it(`${str} → carbon`, function () {
         const { data, output } = boom(str)
