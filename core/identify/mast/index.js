c0680e172a724617eb3a9f93e7ad0a64db8f512d
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-08-06 18:30:23 +0200

    Add identify mast size range

--- core/identify/mast/index.js
+++ core/identify/mast/index.js
@@ -2,9 +2,11 @@
 
 const createDirectoryFlow = require('../../directory/create-flow')
 const { accesories, sails } = require('../../directory')
+const mastRangeSize = require('../../range/mast-size')
 const createAddFactory = require('../create-add')
 const category = require('../../category')
 const carbon = require('./carbon')
+const {get} = require('lodash')
 const size = require('./size')
 const type = require('./type')
 
@@ -13,23 +15,25 @@ const directory = createDirectoryFlow([sails, accesories])
 function factory (log) {
   const createAdd = createAddFactory('mast', log)
 
-  const addCategory = createAdd('category', acc => {
-    return {
-      data: category('masts'),
-      output: acc.input
-    }
-  })
+  const addCategory = createAdd('category', acc => ({
+    data: category('masts'),
+    output: acc.input
+  }))
 
-  const addBrand = createAdd('brand', acc => {
-    return {
-      data: acc.dir.data.brand,
-      output: acc.dir.output
-    }
-  })
+  const addBrand = createAdd('brand', acc => ({
+    data: acc.dir.data.brand,
+    output: acc.dir.output
+  }))
 
   const addType = createAdd('type', acc => type(acc.input))
   const addCarbon = createAdd('carbon', acc => carbon(acc.input))
   const addSize = createAdd('size', acc => size(acc.input))
+
+  const addSizeRange = createAdd('size range', acc => {
+    const size = get(acc, 'data.size')
+    const data = size && mastRangeSize(size)
+    return {data}
+  })
 
   function mast (input) {
     const dir = directory(input)
@@ -40,6 +44,7 @@ function factory (log) {
     addType(acc)
     addCarbon(acc)
     addSize(acc)
+    addSizeRange(acc)
 
     return { data: acc.data, output: acc.input }
   }
