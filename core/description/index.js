632f969a8f8c11ae21032dafa289dcb88f0c5980
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-11-08 09:17:33 +0000

    Use import-modules instead of req-all

--- core/description/index.js
+++ core/description/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { singular: getCategory } = require('../category/index')
-const description = require('req-all')('./category')
+const description = require('import-modules')('./category')
 
 module.exports = doc => {
   const category = getCategory(doc.category)
